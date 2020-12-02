da5fe3c889fc48a9a708382967ba59da335c1239
guhenriofc <you@example.com>
2020-12-02 17:02:48 -0200

    Projeto POOCelular em andamento

---
+++ Classes.cs/Celular.cs
@@ -0,0 +1,13 @@
+namespace POO_CadastroDeAlunos.Classes.cs
+{
+    public class Alunos
+    {
+        public string ligar;
+        public string desligar;
+        public string fazerLigação;
+        public string enviarMensagem;
+
+        
+        
+    }
+}
